8b2e2a8cce9347f43c1ffdb50d7ff76a3489cd12
Annita <e.annita2001@gmail.com>
2025-03-28 01:36:21 +0600

    click events

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,22 @@
 
 
 const Button = () => {
+
+    let count = 0;
+    const handleClick = (name)=> {
+        if (count < 4){
+            count++;
+            console.log(`${name} you clicked me ${count} time(s).`);
+        }
+        else
+        {
+            console.log(`${name} Stop clicking me!`);
+        }
+    }
+
     return (
-        <button className="button">
+        // to make sure handleClick is invoked when button is clicked
+        <button onClick={()=> handleClick("Annita")} className="button">
             {'Click Me!'.toUpperCase()}
         </button>
     )
