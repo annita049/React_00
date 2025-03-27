7696b19f586483fdc456599e3284fa9ce011090f
Annita <e.annita2001@gmail.com>
2025-03-27 21:54:50 +0600

    conditional rendering of elements

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,24 @@
 import List from './Components/List';
 
 function App() {
+  const fruits = [
+    { id: 1, name:"orange", calories: 45},
+    { id: 2, name:"banana", calories: 105},
+    { id: 3, name: "apple", calories: 95}, 
+    { id: 4, name:"dates", calories: 282}, 
+    { id: 5, name:"strawberry", calories: 33}
+  ];
+
+  const vegetables = [{ id: 6, name:"potatoes", calories: 110},
+    { id: 7, name:"celery", calories: 15},
+    { id: 8, name: "carrot", calories: 25}, 
+    { id: 9, name:"broccoli", calories: 50}
+  ];
+
   return (
     <>
-      <List/>
+      {fruits.length > 0 && <List items={fruits} category="Fruits"/>}
+      {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/>}
     </>
   )
 }
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,32 +1,25 @@
-const List = () => {
-    const fruits = [{ id: 1, name:"orange", calories: 45},
-         { id: 2, name:"banana", calories: 105},
-          { id: 3, name: "apple", calories: 95}, 
-          { id: 4, name:"dates", calories: 282}, 
-          { id: 5, name:"strawberry", calories: 33}];
-
+const List = ({items, category}) => {
     
     // fruits.sort((a,b)=> a.name.localeCompare(b.name)); // lexographical ascending order (by names)
 
     // fruits.sort((a,b)=> a.calories - b.calories); // numeric ascending (by calories)
 
-    const lowCalFruits = fruits.filter((fruit)=> fruit.calories < 100);
+    // const lowCalFruits = fruits.filter((fruit)=> fruit.calories < 100);
+    // lowCalFruits.sort((a,b)=> a.calories - b.calories);
 
-    lowCalFruits.sort((a,b)=> a.calories - b.calories);
-
-    const fruitItems = lowCalFruits.map((fruit) => 
-        <li key={fruit.id}>
-            {fruit.name}: &nbsp; <i> {fruit.calories} </i>
+    const Items = items.map((item) => 
+        <li key={item.id}>
+            <i> {item.name}: </i> &nbsp; {item.calories} 
         </li>
     )
 
     // in case two object name property is same , we need a unique "key" prop to distinguish btwn them
     // using name here cause they are unique in this case.
     return (
-        <div>
-            <h1>Low Calory Fruit</h1>
+        <div class="listing">
+            <h3 className="items-category">{category}</h3>
             <ul>
-                {fruitItems}
+                {Items}
             </ul>
         </div>
     )
